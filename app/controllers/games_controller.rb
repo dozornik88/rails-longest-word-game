8cf6bdee013f5bcecffb4720d0afff2a92f6c088
Sergejs Dementjevs <dozornik88@gmail.com>
2019-12-24 17:35:55 +0100

    rails new

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,9 @@
 class GamesController < ApplicationController
   def new
-    @letters = Array.new() { ('A'..'Z').to_a.sample }
-    @letters.save
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
   end
 
   def score
+    @answer = params[:new_question]
   end
 end
